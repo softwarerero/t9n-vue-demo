01830b86b69fcb113b07cbc7c4c6abd41453045d
softwarerero <el@softwarerero.com>
2019-05-06 23:00:02 -0400

    Fix regex

--- src/t9n.js
+++ src/t9n.js
@@ -5,9 +5,9 @@ T9n.map('en', {
     'Current locale': 'Current locale',
     'Welcome to Your Vue.js App': 'Welcome to Your Vue.js App!',
     'sharedPhotos': `@{userName} @{photoCount -> 
-        [0] hasn\'t added any photos yet
-        1 added a new photo
-        [2-4] added a few photos
+        ^0$ hasn\'t added any photos yet
+        ^1$ added a new photo
+        ^[2-4]$ added a few photos
         .* added $photoCount new photos
     }, @{sex -> 
         female she
@@ -20,9 +20,9 @@ T9n.map('es', {
     'Current locale': 'Idioma actual',
     'Welcome to Your Vue.js App': '¡Bienvenidos en tu Vue.js App!',
     'sharedPhotos': `@{userName} @{photoCount -> 
-        [0] toavia no agregó fotos
-        1 agregó una foto
-        [2-4] agregó pocas fotos
+        ^0$ toavia no agregó fotos
+        ^1$ agregó una foto
+        ^[2-4]$ agregó pocas fotos
         .* agregó $photoCount fotos
     }, @{sex -> 
         female ella
@@ -35,9 +35,9 @@ T9n.map('de', {
     'Current locale': 'Aktuelle Sprache',
     'Welcome to Your Vue.js App': 'Deine eigene kleine Vue.js App heißt Dich Willkommen!',
     'sharedPhotos': `@{userName} @{photoCount -> 
-        [0] hat noch kein Photo hochgeladen
-        1 hat ein Photo hochgeladen
-        [2-4] hat ein paar Photos hochgeladen
+        ^0$ hat noch kein Photo hochgeladen
+        ^1$ hat ein Photo hochgeladen
+        ^[2-4]$ hat ein paar Photos hochgeladen
         .* hat $photoCount Photos hochgeladen
     }, @{sex -> 
         female sie
